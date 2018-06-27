8bd962416de6005d975c9417b64e9c1dc4619933
nicoundurraga <nicolas@pingon.cl>
2018-06-27 12:35:49 -0400

    commit

--- app/src/main/java/cl/pingon/flash/views/main/drawer/UploadPhoto.java
+++ app/src/main/java/cl/pingon/flash/views/main/drawer/UploadPhoto.java
@@ -2,9 +2,13 @@ package cl.pingon.flash.views.main.drawer;
 
 import android.content.Context;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -28,19 +32,26 @@ public class UploadPhoto {
         String baseUrl = "gs://flash-a7f9f.appspot.com/avatars/";
         String refUrl = baseUrl + folder + photoName;
         final StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(refUrl);
-        storageReference.putFile(Uri.parse(path)).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+        UploadTask uploadTask = storageReference.putFile(Uri.parse(path));
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
             @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                String[] fullUrl = storageReference.getDownloadUrl().toString().split("&token");
-                String url = fullUrl[0];
-
-                Log.d("PHOTO_URL", url);
-
-                new PhotoPreference(context).photoSave(url);
-
-
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return storageReference.getDownloadUrl();
             }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    String[] parts = task.getResult().toString().split("&token");
+                    String url = parts[0];
+
+                    Log.d("PHOTO_URL", url);
+
+                    new PhotoPreference(context).photoSave(url);
+
+                }
+            }
+
         });
     }
 }
